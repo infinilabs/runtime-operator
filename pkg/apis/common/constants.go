ada56ea45731e623c856c05918e2956177a5a87c
lizhanbin <lizhanbin@qq.com>
2025-05-26 10:31:16 +0800

    fix: update group name to `infini.cloud`

--- pkg/apis/common/constants.go
+++ pkg/apis/common/constants.go
@@ -26,9 +26,9 @@ package common
 
 // Constants for standard labels and operator identification.
 const (
-	AppNameLabel      = "app.infini.cloud/application-name"   // Label for ApplicationDefinition name
-	CompNameLabel     = "app.infini.cloud/component-name"     // Label for ComponentDefinition name (type)
-	CompInstanceLabel = "app.infini.cloud/component-instance" // Label for ApplicationComponent name (instance)
-	ManagedByLabel    = "app.kubernetes.io/managed-by"        // Standard Kubernetes label
-	OperatorName      = "infini-operator"                     // Name of this operator
+	AppNameLabel      = "infini.cloud/application-name"   // Label for ApplicationDefinition name
+	CompNameLabel     = "infini.cloud/component-name"     // Label for ComponentDefinition name (type)
+	CompInstanceLabel = "infini.cloud/component-instance" // Label for ApplicationComponent name (instance)
+	ManagedByLabel    = "app.kubernetes.io/managed-by"    // Standard Kubernetes label
+	OperatorName      = "infini-operator"                 // Name of this operator
 )
